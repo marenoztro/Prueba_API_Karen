03c39d3e130ebd269bea0a8a1f466db0b14834fe
marenoztro <marlon.marotto@gmail.com>
2022-09-23 17:21:39 +0000

    Para Karen

--- src/main.py
+++ src/main.py
@@ -30,6 +30,11 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
+
+
+#OJO: AQUÍ HACEMOS EL endpoint/ruta DE User
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -39,7 +44,140 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+
+#OJO: AQUÍ HACEMOS EL endpoint/ruta DE TODOS LOS Planets
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets= Planets.query.all()
+    
+    results = list(map(lambda item: item.serialize(),planets))
+
+    response_body = {
+        "msg":"Todo creado con exito",
+        "results": results
+    }
+
+    return jsonify(results), 200
+
+
+
+
+#OJO: AQUÍ HACEMOS EL endpoint/ruta ESPECÍFICO DE CADA Planeta
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_one_planet(planet_id):
+    planet = Planets.query.filter_by(id=planet_id).first()
+    print(planet.serialize()) #<Planet 1>
+    # results = list(map(lambda item: item.serialize(),planets))
+
+    response_body = {
+        "msg":"Todo creado con exito",
+        "planet": planet.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+
+
+#OJO: AQUÍ HACEMOS EL endpoint/ruta DE TODOS LOS People
+@app.route('/people', methods=['GET'])
+def get_people():
+    people= People.query.all()
+
+    results = list(map(lambda item: item.serialize(),people))
+
+    response_body = {
+        "msg":"Todo creado con exito",
+        "results": results
+    }
+
+    return jsonify(results), 200
+
+
+
+#OJO: AQUÍ HACEMOS EL endpoint/ruta ESPECÍFICO DE CADA People
+@app.route('/people/<int:people>', methods=['GET'])
+def get_one_people(people_id):
+    people = People.query.filter_by(id=people_id).first()
+    # print(people.serialize()) #<Planet 1>
+    # results = list(map(lambda item: item.serialize(),planets))
+
+    response_body = {
+        "msg":"Todo creado con exito",
+        "people": people.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+    #//////////////// AHORA VAMOS CON LOS POST ////////////////////////
+
+#OJO: AQUÍ HACEMOS EL endpoint/ruta PARA POSTEAR UN People EN Favorites
+@app.route('/favorites/people/<int:people_idd>/<int:user_idd>', methods=['POST'])
+def post_one_people_fav(people_idd, user_idd):
+    id_people = int(people_idd)
+    id_user = int(user_idd)
+    new_fav_people = Favorites(user_id=id_user, people_id= id_people, planet_id=None)
+    db.session.add(new_fav_people) #AQUÍ GREGAMOS A LA db RESULTADO DE LA VARIABLE post_new_fav_people
+    db.session.commit() #AQUÍ COMMITEAMOS/GUARDAMOS TODOS LOS CAMBIOS EN EL db
+
+    response_body = {
+        "msg":"Se ha agregado el personaje ",
+        "query_people": new_fav_people.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #OJO: AQUÍ HACEMOS EL endpoint/ruta PARA POSTEAR UN People EN Favorites
+# @app.route('/favorite/people/<int:people_id>/<int:user_id>', methods=['POST'])
+# # def post_one_people_fav(people_id):
+# #     query_people_fav = Favorites(people_id=body["people_id"], user_id=body["user_id"])
+#     db.session.add(post_one_people_fav) #AQUÍ GREGAMOS A LA db RESULTADO DE LA VARIABLE post_new_fav_people
+#     db.session.commit() #AQUÍ COMMITEAMOS/GUARDAMOS TODOS LOS CAMBIOS EN EL db
+
+#     response_body = {
+#         "msg":"Se ha agregado el personaje ",
+#         "query_people": query_people_fav.serialize()
+#     }
+
+#     return jsonify(response_body), 200
+
+
+
+# OPCIÓN CHILENA 
+
+# #OJO: AQUÍ HACEMOS EL endpoint/ruta PARA POSTEAR UN People EN Favorites
+# @app.route('/favorite/people/<int:people_id>', methods=['POST'])
+# def post_one_people(people_id):
+#     query_people = People.query.get(people_id) #AQUÍ CONSULTO EL PERSONAJE POR SU people_id
+#     user = User.query.get(1)#AQUÍ CONSULTO EL USUARIO QUE ESTÁ GUARDANDO SUS FAVORITOS
+#     if(query_people): #AQUÍ USO UNA CONDICIONAL PARA CONFIRMAR QUE DICHO PERSONAJE EXISTA Y ESTÉ REGISTRADO... SI ES ASÍ PASO A LAS SIGUIENTES LÍNEAS
+#         post_new_fav_people = Fav_people() #AQUÍ CREO UNA NUEVA VARIABLE/ NUEVO REGISTRO PARA LA TABLA Fav_people()
+#         post_new_fav_people.user.id = user.id #AQUÍ NOS BASAMOS EN LOS CAMPOS DE LA TABLA FAVORITOS Y LE INDICAMOS QUE EN LA PARTE DEL ID DEL USUARIO VA EL user.id
+#         post_new_fav_people.people_id = people_id #AQUÍ NOS BASAMOS EN LOS CAMPOS DE LA TABLA FAVORITOS Y LE INDICAMOS QUE EN LA PARTE DEL ID DEL PERSONAJE VA EL people_id
+#         db.session.add(post_new_fav_people) #AQUÍ GREGAMOS A LA db RESULTADO DE LA VARIABLE post_new_fav_people
+#         db.session.commit() #AQUÍ COMMITEAMOS/GUARDAMOS TODOS LOS CAMBIOS EN EL db
+#     return "Listo! se agregó el personaje"
+
+#     response_body = {
+#         "msg":"Se ha agregado el personaje ",
+#         "query_people": query_people.serialize()
+#     }
+
+#     return jsonify(response_body), 200
--- src/models.py
+++ src/models.py
@@ -6,14 +6,81 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorites', backref='user', lazy=True)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def _repr_(self):
+        return '<User %r>' % self.id
+
+    def serialize(self): #MUCHO OJO LA FUNCIÓN serialize TRANSFORMÁS LA ETIQUETA ILEGIBLE CON QUE TE RESPONDE PYTHON EN UN OBJETO LEGIBLE
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+
+
+
+#OJO: AQUÍ HACEMOS LA TABLA DE Favorites (IMPORTANTE: ESTE TIENE RELACIÓN 1 A MUCHOS -en favortiros pueden haber muchos usuarios, planetas y personajes)
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+
+    def _repr_(self):
+        return '<Favorites %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "people_id": self.people_id,
+            # do not serialize the password, its a security breach
+        }
+
+
+
+
+
+#OJO: AQUÍ HACEMOS LA TABLA DE DE Planets
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites', backref='planets', lazy=True)
+
+
+    def _repr_(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+
+
+
+
+#OJO: AQUÍ HACEMOS LA TABLA DE DE People
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites', backref='people', lazy=True)
+
+
+    def _repr_(self):
+        return '<People %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
             # do not serialize the password, its a security breach
         }
